b7aa7810e1a7231dc17ccf72bda4c97b46c7a42a
p-bray <pbr23869@gmail.com>
2024-09-21 18:13:11 -0400

    progressing through the tutorial

--- src/App.js
+++ src/App.js
@@ -1,5 +1,76 @@
-export default function Square() {
+import { useState } from 'react';
 
-    const value = 3 ;
-    return <button className="square">X + {value}</button>;
+
+function Square() {
+  const [value, setValue] = useState(null); 
+  const [chungus, setChungus] = useState(null);
+  
+  function handleClick(){
+    // console.log("clicked on button with value of  "+ value);
+    setValue("X");
+    setChungus("chung");
   }
+
+  return (
+    <button 
+      class="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+function Row({rowId}) {
+  var id = "rowId"+rowId;
+
+  return <div id={id} class="board-row"></div>;
+}
+
+export default function Board() {
+  return (
+    <>
+      <div class='board-row'>
+        <Square/>
+        <Square/>
+        <Square/>
+      </div>
+      <div class='board-row'>
+        <Square/>
+        <Square/>
+        <Square/>
+      </div>
+      <div class='board-row'>
+        <Square/>
+        <Square/>
+        <Square/>
+      </div>
+    </>
+  );
+  // return <BuildBoard dimensions={3}/>;
+}
+
+
+
+
+//builds the squares of the board
+function BuildBoard(dimensions) {
+  // //3 columns of  3 rows of buttons. 
+  // var board = null;
+
+
+  // //iterate through rows
+  // for (let index = 0; index < dimensions+1; index++) {
+  //   var rowId = "row"+index;
+  //   board += <Row id={index}/>
+    
+  //   //iterate through columns
+  //   for (let index = 0; index < dimensions+1; index++){
+  //     var row = document.getElementById("rowId"+index);
+  //     row += <Square id='1'/>
+  //   }
+    
+  // }
+
+  // return board;
+}
--- src/index.js
+++ src/index.js
@@ -9,6 +9,5 @@ const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
     <App />
-    <Test />
   </StrictMode>
 );
